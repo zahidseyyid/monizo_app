24307f53efa8ad7338d02511133fb798435f6c67
Hamza Zahid <zahidseyyid@gmail.com>
2025-01-01 22:43:42 +0300

    feat(localization): add AppStrings class for localized strings

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,15 @@
+import 'package:easy_localization/easy_localization.dart';
+
+class AppStrings {
+  static String appName = "appName".tr();
+  static String changeLanguage = "changeLanguage".tr();
+  static String total = "total".tr();
+  static String turkish = "turkish".tr();
+  static String tr = "tr";
+  static String english = "english".tr();
+  static String en = "en";
+  static String settings = "settings".tr();
+
+  // Assets paths
+  static String get languagePath => "assets/langs";
+}
