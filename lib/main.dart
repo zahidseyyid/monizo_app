853513a9ea42a16fda7c63051cd1df782ce400f8
Hamza Zahid <zahidseyyid@gmail.com>
2025-01-02 20:03:48 +0300

    feat: refactor main.dart to integrate ScreenUtil for responsive design and update localization path

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:monizo_app/core/constants/app_strings.dart';
+import 'package:monizo_app/core/constants/app_assets.dart';
 import 'package:monizo_app/core/theme/themes.dart';
-
-import 'presentation/views/splash_page.dart';
+import 'package:monizo_app/presentation/views/onboarding_page.dart';
+import 'package:toastification/toastification.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-  runApp(EasyLocalization(
+  runApp(
+    EasyLocalization(
       supportedLocales: [Locale(AppStrings.tr), Locale(AppStrings.en)],
-      path: AppStrings.languagePath,
+      path: AppAssets.languagePath,
       fallbackLocale: Locale(AppStrings.en),
-      child: const MainApp()));
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -20,14 +25,22 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.lightTheme,
-      debugShowCheckedModeBanner: false,
-      title: AppStrings.appName,
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      home: const SplashPage(),
+    return ScreenUtilInit(
+      designSize: const Size(414, 896),
+      minTextAdapt: true,
+      builder: (context, child) {
+        return ToastificationWrapper(
+          child: MaterialApp(
+            theme: AppTheme.lightTheme,
+            debugShowCheckedModeBanner: false,
+            title: AppStrings.appName,
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            home: const OnboardingPage(),
+          ),
+        );
+      },
     );
   }
 }
