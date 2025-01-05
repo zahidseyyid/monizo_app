2f497d3e5e4574061bbcddfbffce2d14b48adc15
Hamza Zahid <zahidseyyid@gmail.com>
2025-01-05 12:35:31 +0300

    feat: add HeaderCurveContainer widget with custom clipping for enhanced UI design

---
+++ lib/presentation/widgets/home_page/header_curve_container.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:monizo_app/core/theme/colors.dart';
+
+class HeaderCurveContainer extends StatelessWidget {
+  const HeaderCurveContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: SmoothCurveClipper(),
+      child: Container(
+        height: 287.h,
+        decoration: const BoxDecoration(
+          gradient: AppColors.primaryGradient,
+        ),
+      ),
+    );
+  }
+}
+
+class SmoothCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final Path path = Path();
+
+    // Alt sol köşe
+    path.lineTo(0, size.height - 45.h);
+
+    // İlk eğri (sol kenar ve orta arasında)
+    path.quadraticBezierTo(
+      size.width * 0.3, size.height - 22.h, // İlk kontrol noktası
+      size.width / 2, size.height - 22.h, // Orta nokta
+    );
+
+    // İkinci eğri (orta ve sağ kenar arasında)
+    path.quadraticBezierTo(
+      size.width * 0.7, size.height - 22.h, // İkinci kontrol noktası
+      size.width, size.height - 45.h, // Sağ alt köşe
+    );
+
+    // Üst köşelere çizgiler
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
