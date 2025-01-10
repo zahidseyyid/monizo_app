8a147eb68eb2a74f9ab807db626786eefc7a6e2a
zahidseyyid <zahidseyyid@gmail.com>
2025-01-10 07:04:10 +0300

    refactor: update theme to use app_colors and enhance text styles with additional headings

--- lib/core/theme/themes.dart
+++ lib/core/theme/themes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:monizo_app/core/theme/colors.dart';
+import 'package:monizo_app/core/theme/app_colors.dart';
 import 'package:monizo_app/core/theme/text_styles.dart';
 
 class AppTheme {
@@ -9,7 +9,11 @@ class AppTheme {
     textTheme: TextTheme(
       displayMedium: AppTextStyles.displayMedium50,
       displaySmall: AppTextStyles.displaySmall36,
+      headlineLarge: AppTextStyles.headlineLarge32,
+      titleLarge: AppTextStyles.titleLarge20,
       titleMedium: AppTextStyles.titleMedium18,
+      titleSmall: AppTextStyles.titleSmall16,
+      labelLarge: AppTextStyles.labelLarge14,
     ),
   );
 }
