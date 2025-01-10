83d74c629498aa85b8ce7ac8553180ad0e0aa4b3
zahidseyyid <zahidseyyid@gmail.com>
2025-01-10 07:02:01 +0300

    refactor: update text styles to use app_colors and enhance color consistency

--- lib/core/extensions/text_style_extensions.dart
+++ lib/core/extensions/text_style_extensions.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:monizo_app/core/theme/colors.dart';
+import 'package:monizo_app/core/theme/app_colors.dart';
 
 extension TextStyleExtensions on BuildContext {
-  TextStyle get displayMedium =>
-      Theme.of(this).textTheme.displayMedium!.copyWith(color: AppColors.white);
+  TextStyle get displayMedium50 => Theme.of(this).textTheme.displayMedium!;
 
-  TextStyle get displaySmall =>
+  TextStyle get displaySmall36 =>
       Theme.of(this).textTheme.displaySmall!.copyWith(color: AppColors.primary);
 
-  TextStyle titleMedium({Color? color}) => Theme.of(this)
-      .textTheme
-      .titleMedium!
-      .copyWith(color: color ?? AppColors.white);
+  TextStyle get headlineLarge32 => Theme.of(this).textTheme.headlineLarge!;
+
+  TextStyle get titleLarge20 =>
+      Theme.of(this).textTheme.titleLarge!.copyWith(color: AppColors.white);
+
+  TextStyle get titleMedium18 =>
+      Theme.of(this).textTheme.titleMedium!.copyWith(color: AppColors.white);
+
+  TextStyle get titleSmall16 =>
+      Theme.of(this).textTheme.titleSmall!.copyWith(color: AppColors.white);
+
+  TextStyle get labelLarge14 => Theme.of(this).textTheme.labelLarge!;
 }
--- lib/core/theme/text_styles.dart
+++ lib/core/theme/text_styles.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:monizo_app/core/theme/app_colors.dart';
 
 class AppTextStyles {
   static TextStyle get displayMedium50 => GoogleFonts.inter(
         fontSize: 50.sp,
         fontWeight: FontWeight.bold,
+        color: AppColors.white,
       );
 
   static TextStyle get displaySmall36 => GoogleFonts.inter(
@@ -13,8 +15,31 @@ class AppTextStyles {
         fontWeight: FontWeight.w700,
       );
 
+  static TextStyle get headlineLarge32 => GoogleFonts.inter(
+        fontSize: 32.sp,
+        fontWeight: FontWeight.w700,
+        color: AppColors.white,
+      );
+
+  static TextStyle get titleLarge20 => GoogleFonts.inter(
+        fontSize: 20.sp,
+        fontWeight: FontWeight.w600,
+        color: AppColors.white,
+      );
+
   static TextStyle get titleMedium18 => GoogleFonts.inter(
         fontSize: 18.sp,
         fontWeight: FontWeight.w600,
+      );
+
+  static TextStyle get titleSmall16 => GoogleFonts.inter(
+        fontSize: 16.sp,
+        fontWeight: FontWeight.w600,
+      );
+
+  static TextStyle get labelLarge14 => GoogleFonts.inter(
+        fontSize: 14.sp,
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
       );
 }
